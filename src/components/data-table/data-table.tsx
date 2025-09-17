79a0e2f621a0db874de7160a856de2a8add25371
Rene Senoner <Senonerrene@gmail.com>
2025-09-17 18:10:12 +0000

    bitte gestalte mit dieses teil schöner morderner das heißst also ohne di

--- src/components/data-table/data-table.tsx
+++ src/components/data-table/data-table.tsx
@@ -67,82 +67,80 @@ export function DataTable<TData, TValue>({
   });
 
   return (
-    <Card>
-      <CardContent className="space-y-4 p-6">
-        <div className="flex items-center">
-          <Input
-            placeholder={filterPlaceholder}
-            value={
-              (table.getColumn(filterColumnId)?.getFilterValue() as string) ?? ''
-            }
-            onChange={(event) =>
-              table.getColumn(filterColumnId)?.setFilterValue(event.target.value)
-            }
-            className="max-w-sm"
-          />
-        </div>
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <TableHead key={header.id}>
-                        {header.isPlaceholder
-                          ? null
-                          : flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                      </TableHead>
-                    );
-                  })}
-                </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {loading ? (
-                 [...Array(5)].map((_, i) => (
-                    <TableRow key={i}>
-                        {columns.map((column, j) => (
-                            <TableCell key={j}>
-                                <Skeleton className="h-6 w-full" />
-                            </TableCell>
-                        ))}
-                    </TableRow>
-                 ))
-              ) : table.getRowModel().rows?.length ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow
-                    key={row.id}
-                    data-state={row.getIsSelected() && 'selected'}
-                  >
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </TableCell>
-                    ))}
+    <div className="space-y-4">
+      <div className="flex items-center">
+        <Input
+          placeholder={filterPlaceholder}
+          value={
+            (table.getColumn(filterColumnId)?.getFilterValue() as string) ?? ''
+          }
+          onChange={(event) =>
+            table.getColumn(filterColumnId)?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {loading ? (
+               [...Array(5)].map((_, i) => (
+                  <TableRow key={i}>
+                      {columns.map((column, j) => (
+                          <TableCell key={j}>
+                              <Skeleton className="h-6 w-full" />
+                          </TableCell>
+                      ))}
                   </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell
-                    colSpan={columns.length}
-                    className="h-24 text-center"
-                  >
-                    Keine Ergebnisse.
-                  </TableCell>
+               ))
+            ) : table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && 'selected'}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
                 </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </div>
-        <DataTablePagination table={table} />
-      </CardContent>
-    </Card>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Keine Ergebnisse.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <DataTablePagination table={table} />
+    </div>
   );
 }
